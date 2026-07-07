d661f2fdd6142dbdc415dc7242129f8efac3371d
furmaan <furmaandeep2527@gmail.com>
2026-07-07 18:41:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.7 MB (74.00%)

---
+++ my-folder/2608-count-the-digits-that-divide-a-number/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countDigits(int num) {
+        int cnt=0;
+        int temp = num;
+       while(temp>0){
+        int digit= temp%10;
+        if(num%digit==0){
+            cnt++;
+        }
+        temp /= 10;
+       }
+        return cnt;
+    }
+};
